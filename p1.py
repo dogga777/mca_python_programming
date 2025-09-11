f24c574c5ff96cd93cda7e738f389c3f116446ab
chandra <chandramoulidogga118@gmail.com>
2025-09-11 12:26:43 +0530

    p1.py

--- p1.py
+++ p1.py
@@ -1,10 +1,13 @@
 from turtle import *
 speed('slowest')
-pencolor("red")
+pencolor("purple")
 pensize(3)
 
 side = 6
 for i in range(side):
+    for i in range(side):
+        fd(50)
+        lt(360/side)
     fd(150)
     lt(360/side)
     
